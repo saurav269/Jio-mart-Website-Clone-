a5963b901e94be9d7358f7c4e7c2d77c49dc153f
AvinashKalmegh <avinashkalmegh93@gmail.com>
2023-01-19 22:38:08 +0530

    navbar

--- project-app/src/App.js
+++ project-app/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
+import MenuBar from './Components/MenuBar';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <MenuBar/>
       <Footer/>
     </div>
   );
---
+++ project-app/src/Components/MenuBar.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/layout'
+import React from 'react'
+
+const MenuBar = () => {
+  return (
+    <Box height={"30px"} w={"100%"} bg={"rgb(235, 233, 233)"}>Menu</Box>
+  )
+}
+
+export default MenuBar
