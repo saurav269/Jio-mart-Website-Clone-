6b9e4379c21fdad09e5628a43a48f978379be2a0
rajeshranjan22 <107463246+rajeshranjan22@users.noreply.github.com>
2023-01-20 22:12:20 +0530

    push

--- project-app/src/App.js
+++ project-app/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Cart from './Components/Cart';
+// import { CartItem } from './Components/CartItem';
 
 function App() {
   return (
     <div className="App">
-      <Cart/>
+      <Cart/> 
+       {/* <CartItem/> */}
+      
+      
       
     </div>
   );
