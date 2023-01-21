659f431c71877a97a638a79a015b6af433984fbd
saurabhpawar1997 <saurabhpawarrrr@gmail.com>
2023-01-21 21:47:34 +0530

    day-4

--- project-app/src/App.js
+++ project-app/src/App.js
@@ -4,7 +4,7 @@ import SingleCart from './Components/SingleCart';
 function App() {
   return (
     <div className="App">
-      <SingleCart/>
+      <SingleCart />
     </div>
   );
 }
--- project-app/src/Components/SingleCart.jsx
+++ project-app/src/Components/SingleCart.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import'./SingleCart.css';
 
-const SingleCart = () => {
+
+const SingleCart = (props) => {
   return (
-    <div>
-      
+    <div className='parent'>
+      <div className='child1'>
+        <img src="https://www.jiomart.com/images/product/420x420/493664804/xiaomi-redmi-note-12-pro-5g-128-gb-6-gb-ram-frosted-blue-mobile-phone-digital-o493664804-p597454257-0-202301092110.jpeg" alt="lauda" width='60%' height = '60%'/>
+      </div>
+      <div className='child2'>
+          <h3>Xiaomi Redmi Note 12 Pro 5G 128 GB, 6 GB RAM, Frosted Blue, Mobile Phone</h3>
+          <h3>₹ 24999.00 M.R.P: ₹ 27999.00</h3>
+          <button>Add to Cart</button>
+      </div>
     </div>
   )
 }
