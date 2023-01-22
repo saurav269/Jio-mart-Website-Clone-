a5f712f01fa896eb257edbf531f16fa28b85a7d3
saurabhpawar1997 <saurabhpawarrrr@gmail.com>
2023-01-22 18:17:17 +0530

    day-4

--- project-app/src/Components/SingleCart.jsx
+++ project-app/src/Components/SingleCart.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import'./SingleCart.css';
 
+const dummy = {
+   image : "https://www.jiomart.com/images/product/420x420/493664804/xiaomi-redmi-note-12-pro-5g-128-gb-6-gb-ram-frosted-blue-mobile-phone-digital-o493664804-p597454257-0-202301092110.jpeg",
+   description : 'Xiaomi Redmi Note 12 Pro 5G 128 GB, 6 GB RAM, Frosted Blue, Mobile Phone',
+   price : '₹ 24999.00 M.R.P: ₹ 27999.00'
+}
 
-const SingleCart = (props) => {
+const SingleCart = () => {
+  let cart = JSON.parse(localStorage.getItem("CartData")) || []
+  const addToCart = () => {
+    cart.push(dummy);
+    localStorage.setItem('CartData', JSON.stringify(cart));
+    // cart.find((el) => el.id === data.id && setQuantity(el.qty))
+}
+
+  
   return (
     <div className='parent'>
       <div className='child1'>
-        <img src="https://www.jiomart.com/images/product/420x420/493664804/xiaomi-redmi-note-12-pro-5g-128-gb-6-gb-ram-frosted-blue-mobile-phone-digital-o493664804-p597454257-0-202301092110.jpeg" alt="lauda" width='60%' height = '60%'/>
+        <img src={dummy.image} alt="image" width='60%' height = '60%'/>
       </div>
       <div className='child2'>
-          <h3>Xiaomi Redmi Note 12 Pro 5G 128 GB, 6 GB RAM, Frosted Blue, Mobile Phone</h3>
-          <h3>₹ 24999.00 M.R.P: ₹ 27999.00</h3>
-          <button>Add to Cart</button>
+          <h3>{dummy.description}</h3>
+          <h3>{dummy.price}</h3>
+          <p>Rating : </p>
+          <div>
+          <button onClick={addToCart}>Add to Cart</button>
+          
+          <button >Buy Now</button>
+          </div>
       </div>
     </div>
   )
